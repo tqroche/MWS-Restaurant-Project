1f004b995dd8d28123e86ce7e87a39fdae59fad5
Tchanavian Roche <54754871+tqroche@users.noreply.github.com>
2019-12-18 21:22:26 -0500

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoidHFyb2NoZSIsImEiOiJjazRidnZvbGkwaTBrM3Vud3llcm5qNXRlIn0.J84GvAI0I4wFk3tOLAyE5g',
+        mapboxToken: 'pk.eyJ1IjoidHFyb2NoZSIsImEiOiJjazRieXJzMmwwaW1rM2twZTFmemR2ejZyIn0.90UdREhXktzYo9Ks05T7Vg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
