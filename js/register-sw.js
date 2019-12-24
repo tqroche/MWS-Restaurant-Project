2a47d76801dd14846bb38a9354d0d4b9ef920276
Tchanavian Roche <54754871+tqroche@users.noreply.github.com>
2019-12-24 01:00:45 -0500

    Update register-sw.js

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,8 +1,11 @@
-
-if (navigator.serviceWorker) {
-   navigator.serviceWorker.register('/sw.js').then(function) {
-     console.log("Service Worker has been registered successfully!");
-   }).catch(e) => {
-      console.log("Couldn't register service worker... \n", e);
-   });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
