1fcecb09d980e8e81d64d48a93cbed084c0ce828
Tchanavian Roche <54754871+tqroche@users.noreply.github.com>
2019-12-24 01:04:48 -0500

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,91 +1,59 @@
-const appName = "restaurant-reviews"
-const staticCacheName = appName + "-v1.0";
-
-const contentImgsCache = appName + "-images";
-
-var allCaches = [
-  staticCacheName,
-  contentImgsCache
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
 ];
 
-self.addEventListener('install', function(event) {
-   event.waitUntil(
-     caches.open(staticCacheName).then(function(cache) {
-       return cache.addAll ([
-       '/', // caches index.html
-       'restaurant.html',
-       '/css/styles.css',
-       // other css files are added here
-       '/js/dbhelper.js',
-       '/js/secrect.js',
-       '/js/main.js',
-       '/js/restaurant_info.js',
-       'data/restaurants.json'
-       // add other static assets here like logos, svg icons or any
-       // other asset needed for your app UI
-       // (Don't add restaurant images, as they are not part of your
-       // application's UI)
-      ]);
-    })
-   );
-});
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith(appName) &&
-          		 !allCaches.includes(cacheName);
-        }).map(function(cacheName) {
-           return caches.delete(cacheName);
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll(cacheFiles);
         })
-      );
-     })
-   );
+    );
 });
 
-/**Hijack fetch requests and respond accordingly */
-self.addEventListener('fetch', function(event) {
-  const requestUrl = new URL(event.request.url);
-	
-// only highjack request made to our app (not mapbox maps or leaflet, for example)	
-if (requestUrl.origin === location.origin) {
-
-  // Since requests made to restaurant.html have search params (like ?id=1), the url can't be used as the
-  // key to access the cache, so just respondWith restaurant.html if pathname startsWith '/restaurant/html'
-  if (requestUrl.pathname.startsWith('/restaurant.html')) {
-    event.respondWith(caches.match('/restaurants.html'));
-    return; // Done handling request, so exit early.
-  }
-  
-// If the request pathname starts with/img, then we need to handle images.
-if (requestUrl.pathname.startsWith('/img')) {
-    event.respondWith(serveImage(event.request));
-    return; // Done handling request, so exit early.
-  }  
-}
-
-// Default behavior: respond with cached elements, if any, falling back to network.
-event.respondWith(
-  caches.match(event.request).then(function(response) {
-    return respone || fetch(event.request);
-     })
-   );
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            if (response) {
+                console.log('Found ', e.request, ' in cache');
+                return response;
+            }
+            else {
+                console.log('Could not find ', e.request, ' in cache, FETCHING!');
+                return fetch(e.request)
+                .then(function(response) {
+                    const clonedResponse = response.clone();
+                    caches.open('v1').then(function(cache) {
+                        cache.put(e.request, clonedResponse);
+                    })
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                });
+            }
+        })
+    );
 });
-
-function serveImage(request) {
-  let imageStorageUrl = request.url;
-  
-  imageStorageUrl = imageStorageUrl.replace(/-small\. \w{3}|-medium\. \w{3}|-large\. \w{3}/i, '');
-  
-  return caches.open(contentImgsCache).then(function(cache) {
-    return cache.match(imageStorageUrl).then(function(response) {
-    
-      return respone || fetch(request).then(function(networkResponse) {
-        cache.put(imageStorageUrl), networkResponse.clone();
-        return networkResponse;
-      });
-    });
-  });
-}
