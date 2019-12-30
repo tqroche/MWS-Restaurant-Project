034b7dc8dfb9d9a5c054d6729f28609116265550
Tchanavian Roche <54754871+tqroche@users.noreply.github.com>
2019-12-29 21:46:15 -0500

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  imageObject.alt = 'text';
+  image.alt = 'restaurant';
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
